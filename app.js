326e1042781d6f8afaae992b45aebb7e90d15a95
Cairo Stewart <stewartcairo@gmail.com>
2015-12-26 13:49:34 -0800

    in process of adding search feature to app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express'),
 app = express(),
 routes = require('./controllers/routes'),
-morgan = require('morgan');
+morgan = require('morgan'),
+bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/static', express.static(__dirname + '/public'));
 app.use(morgan('combined'));
 
--- controllers/routes.js
+++ controllers/routes.js
@@ -5,27 +5,49 @@ request = require('request'),
 fs = require('fs');
 
 router.get('/',function(req,res) {
-	// request(spartanPayload).pipe(fs.createWriteStream('public/emblem.png'));
-	// request(emblemPayload).pipe(fs.createWriteStream('public/spartan.png'));
-	res.render('pages/index')
+	res.render('pages/index');
 });
 
-var emblemPayload = {
-	url: 'https://www.haloapi.com/profile/h5/profiles/EL Raphiki/emblem',
-	method: 'GET',
-	headers: {
-		'Ocp-Apim-Subscription-Key': api_key,
-		'Content-Type': 'image/png'
+router.post('/',function(req,res) {
+	var tag = req.body.GamerTag;
+	var emblemPayload = {
+		url: 'https://www.haloapi.com/profile/h5/profiles/'+tag+'/emblem',
+		method: 'GET',
+		headers: {
+			'Ocp-Apim-Subscription-Key': api_key,
+			'Content-Type': 'image/png'
+		}
 	}
-}
+	var spartanPayload = {
+		url: 'https://www.haloapi.com/profile/h5/profiles/'+tag+'/spartan',
+		method: 'GET',
+		headers: {
+			'Ocp-Apim-Subscription-Key': api_key,
+			'Content-Type': 'image/png'
+		}
+	}
 
-var spartanPayload = {
-	url: 'https://www.haloapi.com/profile/h5/profiles/EL Raphiki/spartan',
-	method: 'GET',
-	headers: {
-		'Ocp-Apim-Subscription-Key': api_key,
-		'Content-Type': 'image/png'
-	}
-}
+	p1 = new Promise(function(reject, resolve) {
+		request(spartanPayload, function(err, res, body) {
+			if(err) reject(err);
+			fs.writeFileSync('public/spartan.png', body);
+		});
+	});
+
+	p2 = new Promise(function(resolve, reject){
+		request(emblemPayload, function(err,res,body){
+			if(err) reject(err);
+			fs.writeFileSync('public/emblem.png', body);
+		})
+	})
+
+	Promise.all([p1,p2]).then(function(data) {
+		console.log(data)
+		res.redirect('/');
+	}).catch(function(data) {
+		console.log(data);
+	});
+})
+
 
 module.exports = router;
